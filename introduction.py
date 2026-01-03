35d4640eb2fbe0c1ccc5de97b13a5532cdbcb2e4
jimmyluong5 <jimmyluong@yahoo.com>
2026-01-03 12:36:29 -0600

    more changes to day 1

--- introduction.py
+++ introduction.py
@@ -1,10 +1,26 @@
-birth_year = input("Enter your birth year: ")
+#day 1 - intro to python, introduction to the data types
 
-age = 2025 - int(birth_year)
-print(age)
+#printing numbers
+print (1+3) # addition +
+print(3-2) # subtraction
 
-#converting types
-#int()
-#float()
-#bool()
-#str()
+print(3*2) #multiplication
+print(3/2) #integer division
+
+print(3**2) #exponent
+print(3%2) #modulus operator
+
+#checking data types
+print( type(10)) #this should be an integer
+
+print(type(3.14)) #double
+print(type((1+3j))) #complex type
+
+print(type("Jimmy")) #string
+
+print(type([1, 2, 3, 4])) #this is a list/array
+print(type({"name": "Lebron"})) #this is a dictionary or hashmap
+
+print(type({9.8, 1.2, 3})) #this is a set, which is a data structure that stores a collection of items (unordered, mutable and unique and allow for duplicates)
+#mutable objects vs 
+hello world
