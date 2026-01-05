b6bbc87385eab693e9a6942bfb5c1a4432332614
jimmyluong5 <jimmyluong@yahoo.com>
2026-01-05 14:38:44 -0800

    day5

---
+++ day5.py
@@ -0,0 +1,5 @@
+#day 5 - lists
+#a list is just an array from other programming languages
+#but it can hold different data types in one list
+#lists are mutable, meaning you can change the values in the list
+#it can hold strings, ints, floats, etc.
