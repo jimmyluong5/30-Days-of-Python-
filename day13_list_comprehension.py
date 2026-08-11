5eb7725a9cc8efd924ce485674a34a7e2af11aea
jimmyluong5 <jimmyluong@yahoo.com>
2026-08-11 10:03:42 +0800

    day 13 working on list comprehension

---
+++ day13_list_comprehension.py
@@ -0,0 +1,10 @@
+#list comprehension is a way to create a list from a sequence, basically its a short way to create a list
+#list comprehension is considerably faster than using the for loop
+
+#syntax 
+#[expression] for i in iterable if condition]
+
+#for instance if you want to change a string to a list of characters . you can use a couple methods
+
+language = 'Python'
+lst=list(language)
