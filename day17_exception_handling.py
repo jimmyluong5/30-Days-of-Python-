c86b06a4bf798839e02cdf2db7eeb360ccf896d2
jimmyluong5 <jimmyluong@yahoo.com>
2026-08-12 23:57:19 +0800

    finished day 17 mostly useless

--- day17_exception_handling.py
+++ day17_exception_handling.py
@@ -29,8 +29,42 @@ finally:
     pass 
 """ 
 
-try: 
+
+
+"""  try: 
     print(10 + '5')
 except:
     print('Something went wrong')
-    
+    """
+
+
+""" try:
+    name = input('Enter your name:')
+    year_born = input('Year you were born:')
+    age = 2019 - year_born
+    print(f'You are {name}. And your age is {age}.')
+except:
+    print('Something went wrong') """
+
+
+#we can try and determine the problem using excepts
+
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you born:')
+    age = 2019 - int(year_born)
+    print(f'You are {name}. And your age is {age}.')
+except TypeError:
+    print('Type error occur')
+except ValueError:
+    print('Value error occur')
+except ZeroDivisionError:
+    print('zero division error occur')
+else:
+    print('I usually run with the try block')
+finally:
+    print('I always run.')
+
+
+
+
