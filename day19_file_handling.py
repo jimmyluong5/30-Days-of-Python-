ff02570c31cfc53939e42329b90f8b0ee2f1ecc2
jimmyluong5 <jimmyluong@yahoo.com>
2026-08-13 20:00:54 +0800

    appending stuff and writing to files

--- day19_file_handling.py
+++ day19_file_handling.py
@@ -18,7 +18,62 @@ b = binary - used for binary files like images, audio, video, etc
 #opening files for reading
 
 #default mode of open() is reading so we don't need to specify a mode.
-f = open('readingexample.txt')
+f = open('readingexample.txt') #must be in the same folder as the 30 days of python folder.
 print(f.read()) #reads the entire file
 f.close() #closes the file
+
+#f.read() reads the file
+#f.close() closes the file
+
+#instead of printing all the text lets just print the first 5
+f = open('readingexample.txt')
+print(f.read(6))
+f.close()
+
+
+#we can read only the first line
+#using readline()
+
+f = open('readingexample.txt')
+first_line = f.readline()
+print(first_line)
+f.close()
+
+
+#we can read all the lines using readlines()
+#this returns a list of lines
+f = open('readingexample.txt')
+lines = f.readlines()
+print(lines)
+f.close() 
+#the lines of text are separated by \n
+
+#another way to get all the lines as a list is using splitlines()
+f = open('readingexample.txt')
+lines = f.read().splitlines()
+print(lines)
+f.close()
+
+#after we open a file we should close it. there is a new way to open files using (with)
+#which closes the file by its self
+with open('readingexample.txt') as f:
+    lines = f.read().splitlines()
+    print(lines)
+
+
+
+#now we can open files for writing and updating
+
+#to write to an existing file we must add a mode as a parameter to the open function as w - write, 
+
+with open('readingexample.txt', 'a') as f:
+    f.write('This text will be appended to the end') #don't forget the mode
+
+with open('newfile.txt', 'w') as f: #this creates a new text file in the folder of 30 days of Python.
+    f.write('Lebron James and the 76ers')
+    f.write('Steph Curry and the Golden State Warriors')
+
+    
+
+
 
