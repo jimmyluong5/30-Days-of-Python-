8c75d9e095156e0e15325f79c26b9c55da1803fb
jimmyluong5 <jimmyluong@yahoo.com>
2026-08-13 20:43:33 +0800

    started day 20

---
+++ day20_package_manager.py
@@ -0,0 +1,22 @@
+#package manager
+#Python PIP python package manager 
+#PIP - stands for Preferred Installer Program
+
+#to upgrade pip
+
+#pip install --upgrade pip
+#we use pip to install different Python modules 
+#Package is a Python module that can contain one or more modules or other packages
+
+#a moduleor modules that we can install to our application is a package. In programming,
+#we dont have to code everything we can just install packages and import them to our applications.PythonFinalizationError
+
+# install numpy by doing 
+#pip install numpy - Numpy stands for numeric python 
+""" NumPy is the fundamental package for scientific computing with Python. It contains among other things:
+a powerful N-dimensional array object
+sophisticated (broadcasting) functions
+tools for integrating C/C++ and Fortran code
+useful linear algebra, Fourier transform, and random number capabilities """
+
+
