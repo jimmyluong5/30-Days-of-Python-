529ad2a9e49a0dae0fb5f62e651c6e5c44865670
jimmyluong5 <jimmyluong@yahoo.com>
2026-08-25 23:53:42 +0800

    saving

--- day28_APIs.py
+++ day28_APIs.py
@@ -1,1 +1,13 @@
 #apis
+#haven't touched this shit in a minute, we finna learn about APIs which is important
+
+#API - application programming interface(API)
+#in this section we finna cover web APIs 
+#think of APIs that are digital messengers that allow different apps to talk to each other and share data.try
+
+#web apis are specific bridges used on the internet, like how social media platforms share content 
+#across different websites and apps
+
+
+#how it works
+#has a set of rules for how to ask for information 
